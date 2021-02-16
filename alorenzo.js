fa5e25a2493f59ccce31ce212c8e752a6b6adf45
alorenzoti <78368326+alorenzoti@users.noreply.github.com>
2021-02-16 20:02:31 +0100

    16022021

--- alorenzo.js
+++ alorenzo.js
@@ -1,4 +1,8 @@
 function imprim(){
     window.print()
 }
-
+function dis(param){
+   const test= document.getElementById(param)
+   test.style.display = "block";
+    // console.log(document.getElementById(ram))
+}
